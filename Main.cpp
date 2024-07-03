3c16195603aa69e46f0d21cc12ad413e41f25924
zwwkk <137834838+zwwkk@users.noreply.github.com>
2024-07-03 23:46:55 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	std::cout << "Entrer votre calcul : ";
+	std::cin >> a >> b;
+	std::cin.ignore();
+
+	std::cout << "resultat 1 + resultat 2 = " << a + b << std::endl;
+	std::cin.ignore();
+
+	return 0;
+}
